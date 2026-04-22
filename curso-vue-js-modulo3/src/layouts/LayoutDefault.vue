5d7d019617679ae2c11f61bb7e9692a0a1c93e8c
Cleiton Cunha <cleiton_sc_04@hotmail.com>
2026-04-22 20:54:29 -0300

    Criando projeto para aprender a usar componentes

---
+++ curso-vue-js-modulo3/src/layouts/LayoutDefault.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <div class="navbar bg-neutral text-neutral-content">
+            <button class="btn btn-ghost text-xl">daisyUI</button>
+        </div>
+        <RouterView />
+    </div>
+</template>
---
+++ curso-vue-js-modulo3/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+  const router = useRouter()
+
+  function handleAbout() {
+    router.push('/about')
+  }
+
+</script>
+
+<template>
+  <main class="p-10">
+    <button class="btn btn-primary" @click="handleAbout()">
+      About
+    </button>
+  </main> 
+</template>
